5ab9b43d2f6a99fb3bb175052d6ad04652f145d6
Luis_Henao <roronoazoro3704@gmail.com>
2025-04-25 12:56:32 -0500

    feat: :sparkles: day 5 completed

---
+++ demo/src/main/java/com/luis/demo_jpa/domain/Person.java
@@ -0,0 +1,70 @@
+package com.luis.demo_jpa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "personas")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    
+    private String lastname;
+    
+    @Column(name = "programingLanguage")
+    private String language;
+
+    public Person() {
+    }
+
+    public Person(long id, String name, String lastname, String language) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.language = language;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+
+    
+
+}
---
+++ demo/src/main/java/com/luis/demo_jpa/repository/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.luis.demo_jpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luis.demo_jpa.domain.Person;
+
+public interface PersonRepository extends JpaRepository<Person,Long>{  
+    List<Person> findByNameContains(String name);
+    List<Person> findByLanguageEquals(String name);
+    List<Person> findById(long id);;
+
+}
